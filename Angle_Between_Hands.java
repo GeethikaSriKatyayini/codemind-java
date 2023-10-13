08a13102bdd4ddd52637e8e82ad02ae9d0763775
GeethikaSriKatyayini <123355933+GeethikaSriKatyayini@users.noreply.github.com>
2023-10-13 10:49:29 +0530

    import java.util.Scanner; public class angle {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int hh,mm;         double angle;         String[] input;         input =sc.nextLine().split(":");         hh=Integer.parseInt(input[0]);         mm=Integer.parseInt(input[1]);         angle=Math.abs(30*hh-((11*mm)/2.0));         if(angle<=180)         {             System.out.println(angle);         }         else         {             System.out.println(360-angle);         }     } }

---
+++ Angle_Between_Hands.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class angle
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int hh,mm;
+        double angle;
+        String[] input;
+        input =sc.nextLine().split(":");
+        hh=Integer.parseInt(input[0]);
+        mm=Integer.parseInt(input[1]);
+        angle=Math.abs(30*hh-((11*mm)/2.0));
+        if(angle<=180)
+        {
+            System.out.println(angle);
+        }
+        else
+        {
+            System.out.println(360-angle);
+        }
+    }
+}
